f7fd4ce7deafd2d6b0fe6542e6b8f219faabd390
Pauline Whang <paulinewhang@wifi_stu-10-38-77-47.uncc.edu>
2017-03-16 18:32:03 -0400

    first push of crystals

---
+++ assets/game.js
@@ -0,0 +1,27 @@
+var crystal = {
+	blue:
+	{
+		name : "Blue",
+		value: 0
+	},
+	green:
+	{
+		name: "Green",
+		value: 0
+	},
+	red:
+	{
+		name: "Red",
+		value: 0
+	},
+	yellow:
+	{
+		name: "Yellow",
+		value: 0
+	}
+};
+
+
+
+
+
